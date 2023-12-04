686742302c854b3a360637291ebda67e1586dcfa
xZenon <xzenon@gmail.com>
2023-12-04 20:16:59 +0200

    Update Package.swift - set iOS 14 as minimal deployment version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "recurly-client-ios",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14)
     ],
     products: [
         .library(
